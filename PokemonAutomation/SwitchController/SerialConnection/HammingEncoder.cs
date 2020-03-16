8256a244488c23eb17ffb54b00de125e315710f0
GC <_@gc6.jp>
2020-03-17 02:11:03 +0900

    ハミング符号追加

---
+++ PokemonAutomation/SwitchController/SerialConnection/HammingEncoder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SwitchController.SerialConnection
+{
+    /*
+     * from https://en.wikipedia.org/wiki/Hamming_code
+     */
+    public static class HammingEncoder
+    {
+        static readonly int g0 = 0b1101;
+        static readonly int g1 = 0b1011;
+        static readonly int g2 = 0b1000;
+        static readonly int g3 = 0b0111;
+        static readonly int g4 = 0b0100;
+        static readonly int g5 = 0b0010;
+        static readonly int g6 = 0b0001;
+        static readonly int g7 = 0b1110;
+        static readonly int[] G = new int[] { g7, g6, g5, g4, g3, g2, g1, g0 };
+
+        static readonly int h0 = 0b10101010;
+        static readonly int h1 = 0b01100110;
+        static readonly int h2 = 0b00011110;
+        static readonly int h3 = 0b11111111;
+        static readonly int[] H = new int[] { h3, h2, h1, h0 };
+
+        private static int Mul(int[] M, int x, int columns)
+        {
+            var Mix = M.Select(Mi => x & Mi).ToArray();
+            return (byte)Enumerable.Range(0, columns).Select(i =>
+            {
+                return Mix
+                .Select((m, j) => ((m >> i) & 1) << j)
+                .Aggregate((p, q) => p | q);
+            })
+            .Aggregate((p, q) => p ^ q);
+        }
+
+        public static IEnumerable<byte> Encode(IEnumerable<byte> data) =>
+            data.SelectMany(x => (new int[] { ((x & 0xf0) >> 4), (x & 0x0f) }).Select(y => (byte)Mul(G, y, 4)));
+
+        private static byte SampleData(byte x)
+        {
+            return (byte)(((x & 0b00100000) >> 2) | ((x & 0b1110) >> 1));
+        }
+
+        public static bool TryDecodeHalfByte(byte x, out byte decoded)
+        {
+            var Hx = Mul(H, x, 8);
+            if (Hx == 0)
+            {
+                decoded = SampleData(x);
+                return true;
+            }
+
+            // 誤り
+            int find = H.Select((h, i) => h ^ ((Hx & (1 << i)) != 0 ? 0xff : 0x00))
+                .Aggregate((p, q) => p | q);
+
+            var isZero = Enumerable.Range(0, 8)
+                .Select(i =>
+                {
+                    return (find & (1 << i)) == 0 ? i : (int?)null;
+                })
+                .Where(i => i != null)
+                .Cast<int>()
+                .ToArray();
+
+            if (isZero.Count() == 1)
+            {
+                // 修正
+                decoded = SampleData((byte)(x ^ (1 << isZero.Single())));
+                return true;
+            }
+
+            // 2誤り
+            decoded = default;
+            return false;
+        }
+    }
+}
---
+++ PokemonAutomation/SwitchControllerTest/HammingEncoderTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwitchController.SerialConnection;
+using System.Linq;
+
+namespace SwitchControllerTest
+{
+    [TestClass]
+    public class HammingEncoderTest
+    {
+        [TestMethod]
+        public void TestEncode()
+        {
+            var val = HammingEncoder.Encode(new byte[] { 0b00001011 }).ToArray();
+            Assert.AreEqual(2, val.Count());
+            Assert.AreEqual(0b00000000, val.Skip(0).Take(1).FirstOrDefault());
+            Assert.AreEqual(0b01100110, val.Skip(1).Take(1).FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void TestDecode()
+        {
+            var original = (byte)0b00001011;
+            var x = HammingEncoder.Encode(new byte[] { original }).Skip(1).First();
+
+            var success = HammingEncoder.TryDecodeHalfByte(x, out byte decoded);
+            Assert.IsTrue(success);
+            Assert.AreEqual(original, decoded);
+
+            // 1誤り
+            foreach (int i in Enumerable.Range(0, 8))
+            {
+                var y = (byte)(x ^ (1 << i));
+
+                var success2 = HammingEncoder.TryDecodeHalfByte(y, out byte decoded2);
+                Assert.IsTrue(success2);
+                Assert.AreEqual(original, decoded2);
+
+                // 2誤り
+                foreach (int j in Enumerable.Range(0, 8))
+                {
+                    if (i == j)
+                        continue;
+                    var z = (byte)(y ^ (1 << j));
+
+                    var success3 = HammingEncoder.TryDecodeHalfByte(z, out byte decoded3);
+                    Assert.IsFalse(success3);
+                }
+            }
+        }
+    }
+}
